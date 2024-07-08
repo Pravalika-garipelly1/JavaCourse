590bb9d23544f4eda93665c4483f862837783176
Pravalika <pravalikagoud.garipelly@gmail.com>
2024-07-08 13:31:24 -0500

    Program demonstrates the use of For Loop

---
+++ ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop {
+    public static void main(String[] args){
+        int i;
+        for(i=0;i<=4;i++)
+        {
+            System.out.println("i = " + i);
+        }
+    }
+}
