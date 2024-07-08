a59ce4fe71df481da3846b0bc4e7663ffc568b01
Pravalika <pravalikagoud.garipelly@gmail.com>
2024-07-08 00:32:00 -0500

    Program demonstrates the use of various Relational operators

---
+++ RelationalOperators.java
@@ -0,0 +1,21 @@
+public class RelationalOperators {
+    public static void main (String[] args){
+        int x = 5;
+        int y = 10;
+
+        // Equal to 
+        System.out.println("x==y :" + (x==y)); //false
+        // Not Equal to 
+        System.out.println("x!=y :" + (x!=y)); // true
+        // greator than
+        System.out.println("x>y:" + (x>y)); // false
+        // less than 
+        System.out.println("x<y:" + (x<y)); // true
+        // Greater than or equal to
+        System.out.println("x >= y: " + (x >= y));  // false 
+
+        // Less than or equal to    
+        System.out.println("x <= y: " + (x <= y));  // true
+        
+    }
+}
