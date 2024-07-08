473c0e193b6ab127e1872a3a988b39a30af83323
Pravalika <pravalikagoud.garipelly@gmail.com>
2024-07-08 00:43:47 -0500

    Program demonstrates on usage of If-Else Condition Statement

---
+++ IfElseCondition.java
@@ -0,0 +1,12 @@
+public class IfElseCondition {
+    public static void main(String args[]){
+        int marks= 90;
+        if (marks >=90){
+            System.out.println("Merit");
+        }
+        else {
+            System.out.println("Outstanding");
+        }
+    }
+    
+}
