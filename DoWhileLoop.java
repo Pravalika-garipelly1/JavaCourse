4d625a797b6240d18939d0afeaa6c88b566d54da
Pravalika <pravalikagoud.garipelly@gmail.com>
2024-07-08 13:27:29 -0500

    Program demonstrates the use of Do while loop

---
+++ DoWhileLoop.java
@@ -0,0 +1,9 @@
+public class DoWhileLoop {
+    public static void main (String[] args){
+        int a =4;
+        do{
+            System.out.println("a = "+a);
+            a++;
+        }while(a<=6);
+    }
+}
